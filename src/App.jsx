e27a306be97c6077acc1e0257184741b9442ff77
ThunderRabbit <zuzazibubabi@gmail.com>
2025-01-25 05:34:00 +0800

    added a dummy array for testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,30 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 function App() {
-  const [productCollection, setProductCollection] = useState([]);
+  // const [productCollection, setProductCollection] = useState([]);
 
-  const addProduct = (product) => {
-    setProductCollection([...productCollection, product]);
-    console.log(productCollection);
+  // const addProduct = (product) => {
+  //   setProductCollection([...productCollection, product]);
+  //   console.log(productCollection);
+  // };
+
+  const product1 = {
+    name: "Air Max 97",
+    price: 20.99,
+    inCart: false,
+    checkedOut: false,
   };
 
-  const product = {
-    name: "React Presto",
-    price: "20.99",
-  };
+  const productCollection = [
+    product1,
+    product1,
+    product1,
+    product1,
+    product1,
+    product1,
+  ];
+
+  console.log(productCollection);
 
   return (
     <>
