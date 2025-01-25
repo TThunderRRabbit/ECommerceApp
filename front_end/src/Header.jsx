618af2a57c9416db76356779c34c2cc67e39c995
ThunderRabbit <zuzazibubabi@gmail.com>
2025-01-26 04:05:30 +0800

    started working on frontend first

--- front_end/src/Header.jsx
+++ front_end/src/Header.jsx
@@ -4,13 +4,25 @@ export default function Header() {
   return (
     <>
       <div className="header">
-        <div className="navigation">navi</div>
-        <div className="search">
-          <div className="div">search icon</div>
-          <input></input>
+        <div className="navigation icon-padding">
+          <span class="material-symbols-rounded menu icon">menu</span>
         </div>
-        <div className="cart">cart</div>
-        <div className="message">messsage</div>
+        <div className="splitHeader">
+          <div className="search">
+            <div className="div icon-padding">
+              <span class="material-symbols-rounded search icon">search</span>
+            </div>
+            <input className="searchInput"></input>
+          </div>
+          <div className="cart">
+            <span class="material-symbols-rounded cart icon">
+              shopping_cart
+            </span>
+          </div>
+          <div className="message">
+            <span class="material-symbols-rounded forum icon">forum</span>
+          </div>
+        </div>
       </div>
     </>
   );
