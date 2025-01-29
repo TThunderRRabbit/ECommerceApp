fd91016bfee1d999989063a85b54c24bcea84709
ThunderRabbit <zuzazibubabi@gmail.com>
2025-01-29 10:37:30 +0800

    finished the design including the scrollbar

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -74,6 +74,18 @@ function App() {
     product4,
     product5,
     product6,
+    product1,
+    product2,
+    product3,
+    product4,
+    product5,
+    product6,
+    product1,
+    product2,
+    product3,
+    product4,
+    product5,
+    product6,
   ];
 
   console.log(productCollection);
