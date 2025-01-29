560234502710af4a94dcedbc6876272dd25515c5
ThunderRabbit <zuzazibubabi@gmail.com>
2025-01-29 09:35:40 +0800

    add grod designs to product

--- front_end/src/ProductsDisplay.jsx
+++ front_end/src/ProductsDisplay.jsx
@@ -7,7 +7,7 @@ export default function ProductsDisplay({ products }) {
         <div className="productCard" key={index}>
           <img src={product.img} className="productImage" />
 
-          <div className="productName">${product.name}</div>
+          <div className="productName">{product.name}</div>
           <div className="productPrice">${product.price}</div>
         </div>
       ))}
